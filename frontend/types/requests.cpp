e24014520f710402d3aed77767769b86f2b004b9
Kai Kosades <red4truck@tutanota.com>
2021-09-30 03:04:49 +0300

    Created Request class and its children

---
+++ frontend/types/requests.cpp
@@ -0,0 +1,82 @@
+#include "requests.h"
+
+using namespace BaseTypes;
+
+// SearchQueryRequest class
+QString SearchQuery::toJSONString() {
+  throw std::runtime_error("NOT IMPLEMENTED"); // TODO implement
+}
+
+server::requests::RequestVar SearchQuery::translate() {
+	std::set<unsigned int> favoriteIDs;
+	for(auto id : favoriteIDs){
+	  favoriteIDs.insert(id);
+	}
+
+	std::string course;
+	if(courses.size() > 0) {
+	  course = courses.values()[0].toStdString();
+	}
+	std::string cusine;
+	if(cusines.size() > 0) {
+	  course = cusines.values()[0].toStdString();
+	}
+	std::set<std::string> ingredients;
+	for(auto ingredient : this->ingredients){
+		ingredients.insert(ingredient.toStdString());
+	}
+
+	return server::requests::Find(server::searcher::Criteria(
+					favoriteIDs,
+					searchSubtring.toStdString(),
+					course,
+					cusine,
+					ingredients,
+					searchExclusively
+				), 1);
+}
+
+// AddRecipeRequest class
+QString AddRecipeRequest::toJSONString(){
+  throw std::runtime_error("NOT IMPLEMENTED"); // TODO implement
+}
+
+server::requests::RequestVar AddRecipeRequest::translate() {
+	return server::requests::Add(this->recipeToAdd->translateToServer(), 1);
+}
+
+// EditRecipeRequest class
+QString EditRecipeRequest::toJSONString() {
+  throw std::runtime_error("NOT IMPLEMENTED"); // TODO implement
+}
+
+server::requests::RequestVar EditRecipeRequest::translate() {
+  return server::requests::Edit( this->editedRecipe->translateToServer(), 1);
+}
+
+// RemoveRecipeRequest class
+QString RemoveRecipeRequest::toJSONString(){
+  throw std::runtime_error("NOT IMPLEMENTED"); // TODO implement
+}
+
+server::requests::RequestVar RemoveRecipeRequest::translate() {
+  return server::requests::Remove(recipeIDToRemove, 1);
+}
+
+// GetInitDataRequest
+QString GetInitDataRequest::toJSONString() {
+  throw std::runtime_error("NOT IMPLEMENTED"); // TODO implement
+}
+
+server::requests::RequestVar GetInitDataRequest::translate() {
+  return server::requests::GetInitData(1);
+}
+
+//Error class
+QString Error::toJSONString() {
+  throw std::runtime_error("NOT IMPLEMENTED"); // TODO implement
+}
+
+server::requests::RequestVar Error::translate() {
+  return server::requests::Error(1);
+}
---
+++ frontend/types/requests.h
@@ -0,0 +1,99 @@
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+#include <QString>
+#include <QList>
+#include <QSet>
+#include <QSharedPointer>
+#include "../server/requests/include/requests.h"
+#include "responses.h"
+
+
+// TODO remake Request type to have 2 virtual methods: toString() - for JSON, translate() - for local protocols
+namespace BaseTypes {
+
+class Request {
+public:
+	virtual QString toJSONString() = 0;
+	virtual server::requests::RequestVar translate() = 0;
+
+	virtual ~Request() = 0;
+};
+
+class SearchQuery : public Request {
+	QSet<unsigned int> favoriteIDs;
+	QString searchSubtring;
+	QSet<QString> courses;
+	QSet<QString> cusines;
+	QList<QString> ingredients;
+	bool searchExclusively;
+public:
+	SearchQuery(QSet<unsigned int> _favoriteIDs, QString _name, QSet<QString> _courses,
+				 QSet<QString> _cuisines, QList<QString> _ingredients, bool _searchExclusively) :
+	  favoriteIDs(_favoriteIDs), searchSubtring(_name),
+	  courses(_courses), cusines(_cuisines),
+	  ingredients(_ingredients), searchExclusively(_searchExclusively) {
+	}
+
+	QString toJSONString() override;
+	server::requests::RequestVar translate() override;
+
+	~SearchQuery() override {}
+
+};
+class AddRecipeRequest : public Request {
+private:
+	QSharedPointer<Recipe> recipeToAdd;
+public:
+	AddRecipeRequest(QSharedPointer<Recipe> _recipeToAdd) : recipeToAdd(_recipeToAdd) {}
+
+	QString toJSONString() override;
+	server::requests::RequestVar translate() override;
+
+	~AddRecipeRequest() override {}
+};
+
+class EditRecipeRequest : public Request {
+	QSharedPointer<Recipe> editedRecipe;
+  public:
+	EditRecipeRequest(QSharedPointer<Recipe> _editedRecipe) : editedRecipe(_editedRecipe) {}
+
+	QString toJSONString() override;
+	server::requests::RequestVar translate() override;
+
+	~EditRecipeRequest() override {}
+};
+
+class RemoveRecipeRequest : public Request {
+	unsigned int recipeIDToRemove;
+public:
+	RemoveRecipeRequest(unsigned int _recipeIDToRemove) : recipeIDToRemove(_recipeIDToRemove) {}
+
+	QString toJSONString() override;
+	server::requests::RequestVar translate() override;
+
+	~RemoveRecipeRequest() override {}
+};
+
+class GetInitDataRequest : public Request {
+	QString toJSONString() override;
+	server::requests::RequestVar translate() override;
+
+	~GetInitDataRequest() override;
+};
+
+class Error : public Request {
+public:
+	QString errorMessage;
+	Error(QString msg="") : errorMessage(msg){ }
+
+	QString toJSONString() override;
+	server::requests::RequestVar translate() override;
+
+	~Error() override {}
+};
+
+}
+
+
+#endif // REQUESTS_H
