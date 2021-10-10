04de9325d75ab5c5fc25ed578d0c6c7d94c23801
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2021-10-10 13:56:31 +0300

    Remove duplicated and unnecessary functions.

--- server/database_api/include/database_api.h
+++ server/database_api/include/database_api.h
@@ -54,28 +54,13 @@ private:
     void insertDefaultUnit();
     bool containsDefaultUnit();
 
-    //remove functions delete record only it is not used
-    //add functions add only if it is not exist
-    bool addCourse(string course);
-    bool removeCourse(string course);
-    bool checkCuisine(string cuisine);
-    bool checkCuisineUsage(string cuisine);
-
-    bool addCuisine(string cuisine);
-    bool removeCuisine(string cuisine);
-    bool checkCourse(string course);
-    bool checkCourseUsage(string course);
-
-    bool addIngredient(string ingredient);
-    bool removeIngredient(string ingredient);
-    bool checkIngredient(string ingredient);
-    bool checkIngredientUsage(string ingredient);
-
     enum class ItemType {
         COURSE, CUISINE, INGREDIENT, UNIT
     };
-    bool checkItem(ItemType type, string name);
+    //add functions add only if it is not exist
+    //return true if exists or added
     bool addItem(ItemType type, string name);
+    bool checkItem(ItemType type, string name);
 
     void removeUnusedElements();
 
