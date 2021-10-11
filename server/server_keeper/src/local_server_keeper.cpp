5d0553f5e9f363eeb37570a43e4ef930da4378ff
Rostyslav Paranko <rostyslav.paranko@gmail.com>
2021-10-11 15:51:36 +0300

    Added path-to-database constructor

--- server/server_keeper/src/local_server_keeper.cpp
+++ server/server_keeper/src/local_server_keeper.cpp
@@ -4,7 +4,38 @@
 
 namespace server {
 
-LocalServerKeeper::LocalServerKeeper() {
+LocalServerKeeper::LocalServerKeeper()
+    :
+    dataBase{},
+    initializer{dataBase},
+    searcher{dataBase},
+    modifier{dataBase},
+    handler{initializer, searcher, modifier},
+    localExchange{},
+    receiver{localExchange},
+    sender{localExchange},
+    server{handler, receiver, sender}
+{
+    try {
+        server.start();
+    } catch (std::exception& e) {
+        server.stop();
+        throw;
+    }
+}
+
+LocalServerKeeper::LocalServerKeeper(const std::string& dbPath)
+    :
+    dataBase{dbPath},
+    initializer{dataBase},
+    searcher{dataBase},
+    modifier{dataBase},
+    handler{initializer, searcher, modifier},
+    localExchange{},
+    receiver{localExchange},
+    sender{localExchange},
+    server{handler, receiver, sender}
+{
     try {
         server.start();
     } catch (std::exception& e) {
