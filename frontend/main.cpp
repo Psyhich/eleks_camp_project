f99719c18e20389a33e29795716ef45602f87851
Kai Kosades <red4truck@tutanota.com>
2021-10-17 18:27:10 +0300

    Added auto load for default database Changed main window icon

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,10 +1,20 @@
+#include <QApplication>
+#include <QString>
+#include <QStandardPaths>
+#include <QFile>
+
 #include "mainwindow.h"
 #include "connection_manager.h"
 
-#include <QApplication>
-#include <QString>
-
 int main(int argc, char *argv[]) {
+	// Loading default database from resources to default path if it's not set
+	QString path = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation)[0] + "/cookbook.db";
+	if(!QFile::exists(path)){
+	  QFile defaultDB(":/resources/../data/cookbook.db");
+	  defaultDB.copy(path);
+	  defaultDB.close();
+	}
+
 	Connections::ConnectionManager::getManager();
 
 	QApplication a(argc, argv);
